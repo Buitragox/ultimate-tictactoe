0d02b8cd375fdf0a7a2e85eaa222e20a0e3f48dc
Buitragox <jhoanuitrago@gmail.com>
2023-09-22 12:01:54 -0500

    Add padding and hover color

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -12,7 +12,7 @@
         <div class="grid grid-cols-3 border-4 border-pink-400">
         {#each game.board[0][0].matrix as row, i (i)}
             {#each row as value, j (j)}
-                <button class="col-span-1 border-2 border-white w-20 h-20" 
+                <button class="col-span-1 border-2 border-white w-20 h-20 hover:bg-slate-600" 
                         on:click={() => {game = game.localMove(i, j);}}>
                     <p class="text-3xl text-zinc-50">{value}</p>
                 </button> 
@@ -22,16 +22,18 @@
     </div>
 
     {#if game.isGameFinished()}
-            <p class="grid place-items-center text-xl text-zinc-50">The winner is {game.winner}!</p>
+            <p class="grid place-items-center text-xl text-zinc-50 p-1 py-2">
+                The winner is {game.winner}!
+            </p>
     {/if}
 
-    <div class="flex items-center space-x-4 justify-center">
-        <a class="border-2 border-solid rounded px-4
+    <div class="flex items-center space-x-4 justify-center p-2">
+        <a class="border-2 border-solid rounded px-4 py-1
                  border-green-50 bg-green-600 hover:bg-green-500" href="/">
-            Lets go back
+            Go back
         </a>
 
-        <button class="border-2 border-solid rounded px-4 
+        <button class="border-2 border-solid rounded px-4 py-1
                 border-red-50 bg-red-600 hover:bg-red-500" 
             on:click={() => {game = game.resetGame();}}>
             Reset
