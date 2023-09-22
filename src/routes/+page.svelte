7d5cc25be9da0a9e65531b5dd54e2c0779916e0b
Buitragox <jhoanuitrago@gmail.com>
2023-09-22 12:01:30 -0500

    Add style to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
-<h1>Welcome to tic-tac-toe</h1>
 
-<a href="/play">Lets play</a>
+<div class="grid place-items-center">
+    <h1 class="font-medium text-zinc-50 text-3xl p-2 pb-10">Welcome to tic-tac-toe</h1>
 
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <a class="border-2 border-solid rounded px-4 py-1
+            border-green-50 bg-green-600 hover:bg-green-500" href="/play">
+        Let's play
+    </a>
+
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
